01f36d9793f0490ecfa8933a44cd2c23bd068cae
ScottBang <45381235+ScottBang@users.noreply.github.com>
2022-02-04 20:22:50 +0900

    chapter01 basic_http_server.go

---
+++ chapter01/basic_http_server/basic_http_server.go
@@ -0,0 +1,25 @@
+/*
+Test:
+	url에 이하 주소 입력함. http://localhost:8080/helloworld
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := 8080
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
+
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World\n")
+}
